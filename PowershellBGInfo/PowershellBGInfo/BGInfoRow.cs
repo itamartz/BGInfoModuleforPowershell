e5e26dcf49d322e90d64f152cada33498144a827
itamartz_cp <SND\itamartz_cp>
2015-05-11 09:41:42 +0000

    Start

---
+++ PowershellBGInfo/PowershellBGInfo/BGInfoRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowershellBGInfo
+{
+    public class BGInfoRow
+    {
+
+        public object RowText { get; set; }
+
+        public string FontFamily { get; set; }
+
+        public int FontSize { get; set; }
+
+        public string FontStyle { get; set; }
+    }
+}
